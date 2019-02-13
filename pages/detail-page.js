a33a45b39ebbc8850074f9eaac6ba8272eff078f
DASHA <dariaepaneshnikova@icloud.com>
2019-02-13 11:29:26 +0100

    Created elements with data from the form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,33 @@
+//adding an event handler
 function submitComment() {
-    console.log('Hello!')
+
+    //gathering data
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+
+    //Creating new html elements below
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+
+    //adjust the created elements
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg.value
+
+    //comment.classList.add('')
+    
+    comment.appendChild(h3)
+    console.log(comment, h3)
+
+    document.body.appendChild(comment);
+
+    /*
+    */
+
 }
+
+
+//comment.classList.add('')
+//comment.appendChild('h3', 'p')
